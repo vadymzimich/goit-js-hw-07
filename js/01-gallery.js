a84ebb16f8e3183b3da3ff63a52eeb56ec0dc625
Vadym Zimich <tunego97@gmail.com>
2023-02-20 01:14:03 +0200

    Create gallery with LightBox

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,42 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+const items = [];
+
+galleryItems.forEach((element) => {
+  const galleryItem = document.createElement("div");
+  galleryItem.className = "gallery__item";
+
+  const galleryLink = document.createElement("a");
+  galleryLink.className = "gallery__link";
+  galleryLink.href = element.original;
+  //   console.log(galleryLink);
+  const galleryImage = document.createElement("img");
+  galleryImage.className = "gallery__image";
+  galleryImage.src = element.preview;
+  galleryImage.setAttribute("data-source", element.original);
+  galleryImage.alt = element.description;
+
+  galleryItem.append(galleryLink);
+  galleryLink.append(galleryImage);
+
+  items.push(galleryItem);
+});
+
+gallery.append(...items);
+
+gallery.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  const selectedImg = event.target.getAttribute("data-source");
+  const instance = basicLightbox.create(`
+    <img src = "${selectedImg}" width="800" height="600">
+`);
+
+  instance.show();
+});
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+const items = [];
+
+galleryItems.forEach((element) => {
+  const galleryItem = document.createElement("a");
+  galleryItem.className = "gallery__item";
+  galleryItem.href = element.original;
+
+  const galleryImage = document.createElement("img");
+  galleryImage.className = "gallery__image";
+  galleryImage.src = element.preview;
+  galleryImage.alt = element.description;
+
+  galleryItem.append(galleryImage);
+  items.push(galleryItem);
+});
+
+gallery.append(...items);
+
+new SimpleLightbox(".gallery a", {
+  animationSpeed: 300,
+  scrollZoom: false,
+});
